1f3bf8d12bd251427a89ab44fb42ce98e89f590d
Medhavi-CloudAnalogy <medhavi.anand@cloudanalogy.com>
2026-03-23 16:04:55 +0530

    again push

--- 05notes-App/src/App.jsx
+++ 05notes-App/src/App.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
 function App() {
+
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const [task, settask] = useState([]);
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    let obj = {
+      title: title,
+      desc: desc
+    }
+
+    settask([...task, obj]);
+    setTitle("");
+    setDesc("");
+  }
+
+
+useEffect(() => {
+  console.log(task);
+}, [task]);
+
   return (
-    <div className='h-screen w-full bg-black text-white'> 
-      <form className='flex flex-col justify-between'>
-        <input 
-        className='bg-gray-400 text-black p-2 m-2 rounded-md'
+    <div className='h-screen w-full bg-black text-white p-5'> 
+      <form className='flex justify-between' onSubmit={(e)=>{
+        submithandler(e);
+      }}>
+        <div className='flex flex-col w-1/2'>
+          <input 
+        className='w-full bg-gray-400 text-black p-2 m-2 rounded-md'
         type="text"
         placeholder='Enter Task Heading'
+        value={title}
+        onChange={(e)=>{
+          setTitle(e.target.value);
+        }}
         />
 
-        <input 
-        className='bg-gray-400 text-black p-2 m-2 rounded-md'
+        <textarea 
+        className='h-20 w-full bg-gray-400 text-black p-2 m-2 rounded-md'
         type="text"
         placeholder='Write Details'
+        value={desc}
+        onChange={(e)=>{
+          setDesc(e.target.value);
+        }}
         />
 
         <button
-        className='p-2 bg-white text-black rounded-md m-2'
+        className='w-full p-2 bg-white text-black rounded-md m-2'
         >Add Notes</button>
-        
+        </div>
       </form>
     </div>
   )
